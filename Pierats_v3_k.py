cfa1d69c61123e355915e73e718f6a65f683de06
kionahz <khitz.pi@gmail.com>
2024-01-25 17:57:58 +0100

    k 2501 updated v3

--- Pierats_v3_k.py
+++ Pierats_v3_k.py
@@ -1,10 +1,12 @@
 import streamlit as st
-from helpers import turtle_with_speech_bubble, map_bg
+from helpers_v2_k import turtle_with_speech_bubble, map_bg, click_m1_q1
+# Todo: always update the helpers file to work with the current version
 
 st.set_page_config(
     page_title="PIErats - Productivity Island Expedition",
     page_icon="🐢"
 )
+
 
 # creates a dictionary in a list to fill the speech bubble
 speech_turtle = [
@@ -15,25 +17,22 @@ speech_turtle = [
      }
 ]
 
-# call the function to display the turtle, speech bubble and the first text inside the speech bubble
-turtle_with_speech_bubble(speech_turtle[0]['text'])
-
-
-# onclick="{button["execution"]}" command code to execute a button with html
-# -> we have to figure out how to open further pages with click on a button
 
 # Quest Buttons and Button to open backpack
 buttons = [
-    {"label": "🏳️", "top": "77%", "left": "39%", "font-size": "45px"},  # Button M1 Q1
-    {"label": "🔒", "top": "68%", "left": "47%", "font-size": "45px"},  # Button M1 expl
-    {"label": "🔒", "top": "60%", "left": "54%", "font-size": "45px"},  # Button M1 Q2
-    {"label": "🔒", "top": "53%", "left": "48%", "font-size": "45px"},  # Button M2 Q1
-    {"label": "🔒", "top": "57%", "left": "38%", "font-size": "45px"},  # Button M2 expl
-    {"label": "🔒", "top": "47%", "left": "43%", "font-size": "45px"},  # Button M2 Q2
-    {"label": "🔒", "top": "49%", "left": "57%", "font-size": "45px"},  # Button M3 Q1
-    {"label": "🔒", "top": "33%", "left": "60%", "font-size": "45px"},  # Button M3 expl
-    # {"label": "🔒", "top": "70%", "left": "45%", "font-size": "45px"},  # Button M3 Q2
+    {"label": "🏳️", "top": "77%", "left": "39%", "font-size": "45px", "onclick": click_m1_q1},  # Button M1 Q1
+    {"label": "🔒", "top": "68%", "left": "47%", "font-size": "45px", "onclick": click_m1_q1},  # Button M1 expl
+    {"label": "🔒", "top": "60%", "left": "54%", "font-size": "45px", "onclick": click_m1_q1},  # Button M1 Q2
+    {"label": "🔒", "top": "53%", "left": "48%", "font-size": "45px", "onclick": click_m1_q1},  # Button M2 Q1
+    {"label": "🔒", "top": "57%", "left": "38%", "font-size": "45px", "onclick": click_m1_q1},  # Button M2 expl
+    {"label": "🔒", "top": "47%", "left": "43%", "font-size": "45px", "onclick": click_m1_q1},  # Button M2 Q2
+    {"label": "🔒", "top": "49%", "left": "57%", "font-size": "45px", "onclick": click_m1_q1},  # Button M3 Q1
+    {"label": "🔒", "top": "33%", "left": "60%", "font-size": "45px", "onclick": click_m1_q1},  # Button M3 expl
+    # {"label": "🔒", "top": "70%", "left": "45%", "font-size": "45px", "onclick": click_m1_q1},  # Button M3 Q2
 ]
+
+# call the function to display the turtle, speech bubble and the first text inside the speech bubble
+turtle_with_speech_bubble(speech_turtle[0]['text'])
 
 # call the function to display the map as the background image and insert the buttons on top of it
 map_bg(buttons)
